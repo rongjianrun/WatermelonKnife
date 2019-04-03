6d7e697ce343db3199d4a6c199fed51a5451ebe8
rongjianrun <674807275@qq.com>
2019-04-03 14:48:26 +0800

    添加相关注释

--- app/src/main/java/com/rjr/watermelonknife/WatermelonKnife.java
+++ app/src/main/java/com/rjr/watermelonknife/WatermelonKnife.java
@@ -5,6 +5,10 @@ import android.app.Activity;
 public class WatermelonKnife {
 
     public static void bind(Activity activity) {
+        // 这里为什么要用反射？？？
+        // 因为我们无法预知ViewBinder的实现类
+        // 要根据类名动态创建实例
+        // 换种说法，我们要创建的类还不存在，需要编译后才生成
         String cName = activity.getClass().getCanonicalName() + "$$ViewBinder";
         try {
             Class<?> clazz = Class.forName(cName);
